bd559c51688ce142b0672a98ef5f1ed801fd82cd
Zafar Yab Subhani <61932990+yaab5825@users.noreply.github.com>
2020-04-25 14:53:13 +0200

    for loop

--- Test.java
+++ Test.java
@@ -5,8 +5,10 @@ public class Test
 	
 public static void main (String [] args )
 {
-	
-System.out.println ("Hello Dear");
+	for (int i=1; i<10; i++)
+	{
+	System.out.println ("Hello Dear");
+	}
 
 }
 
